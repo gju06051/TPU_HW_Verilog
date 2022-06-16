7db6b0cfc5dae8bbc891e47fa6fcf52f3370b9fb
gju06051 <gju06051@gmail.com>
2022-06-16 10:59:30 +0900

    Update RF.v

--- RTL/STORAGE/RF.v
+++ RTL/STORAGE/RF.v
@@ -5,12 +5,12 @@ module reg_file #(
     parameter   ADDR_WIDTH = $clog2(DATA_DEPTH)     // ex) DATA_DEPTH = 32, ADDR_WIDTH = 5
     )
     (
-    // port(three ported register file)
-    input                       we,         // write enable
-    input   [ADDR_WIDTH-1:0]    ra1, ra2,   // read address
-    input   [ADDR_WIDTH-1:0]    wa,         // write address
-    input   [DATA_WIDTH-1:0]    wd,         // write data
-    output  [DATA_WIDTH-1:0]    rd1, rd2    // read data
+    // port
+    input                       we,                 // write enable
+    input   [ADDR_WIDTH-1:0]    ra1, ra2,           // read address
+    input   [ADDR_WIDTH-1:0]    wa,                 // write address
+    input   [DATA_WIDTH-1:0]    wd,                 // write data
+    output  [DATA_WIDTH-1:0]    rd1, rd2, rd3       // read data
     );
 
     // wire & reg
@@ -24,8 +24,9 @@ module reg_file #(
     end
     
     // register 0 hardwired to 0
-    // read two ports combiniationally(show ahead)
+    // read ports combiniationally(show ahead)
     assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
     assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
+    assign rd3 = (ra3 != 0) ? rf[ra3] : 0;
 
 endmodule
