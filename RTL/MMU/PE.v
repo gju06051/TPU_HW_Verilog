412ad5cb690745afbcecda40bcbd4bbb6c1dd75f
gju06051 <gju06051@gmail.com>
2022-06-17 18:13:47 +0900

    modify some document

--- RTL/MMU/PE.v
+++ RTL/MMU/PE.v
@@ -3,10 +3,10 @@ module PE #(
     parameter DATA_WIDTH = 8,
     parameter IF_ROW_WIDTH = 15,
     parameter FILTER_ROW_WIDTH = 3
-    // FILL HERE //
     )
     (
     // port
+    
     );
 
     localparam PSUM_WIDTH = IF_ROW_WIDTH - FILTER_ROW_WIDTH - 1;
