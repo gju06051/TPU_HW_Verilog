940b6e1a295124bb5225978358d023f27f5b8f9e
jyk2498 <jyk2498@gmail.com>
2022-07-01 14:01:07 +0900

    Fully_Connected code and C code to generate test file for FC is added

--- RTL/Fully_Connected/SW/test.c
+++ RTL/Fully_Connected/SW/test.c
@@ -1,4 +1,20 @@
-# define _CRT_SECURE_NO_WARNINGS
+// random number generator for Data_Mover_BRAM test
+// 
+// description
+//      this code generate 3 files: 
+//          ref_c_rand_input_node.txt
+//              generate the random number between 0 ~ 256 (because node is 8 bits)
+//              4096 * 4 number generated. so each line have 4 numbers: 8bit * 4 = 32 bit
+//
+//          ref_c_rand_input_weight.txt
+//              same as ref_c_rand_input_node.txt but this numbers considered as weight
+//
+//          ref_c_rand_result
+//              The result of MAC operation of 2 file's numbers.
+//              The first number is result of 1st column of 2 file's number: result[0] = file1[0][0] * file2[0][0] + file1[1][0] * file2[1][0] + file1[2][0] * file2[2][0] ...
+
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
