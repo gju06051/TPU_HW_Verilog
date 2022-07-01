ef238a4bfa6c9d97e89e4afbca0c813baa4bc01e
sg05060 <psh2018314072@gmail.com>
2022-07-02 01:56:44 +0900

    update im2col

--- RTL/HW/Adder/Backup/CLA_16.v
+++ RTL/HW/Adder/Backup/CLA_16.v
@@ -15,10 +15,14 @@ module CLA_16(
     output [15 : 0] sum,
     output cout
 );
-    wire c1, c2, c3;
-
-    CLA4 CLA_inst_1 (.A(A[3 : 0]), .B(B[3 : 0]), .Ci(Ci), .S(sum[3 : 0]), .Co(c1), .PG(), .GG());
-    CLA4 CLA_inst_2 (.A(A[7 : 4]), .B(B[7 : 4]), .Ci(c1), .S(sum[7 : 4]), .Co(c1), .PG(), .GG());
-    CLA4 CLA_inst_3 (.A(A[11 : 8]), .B(B[11 : 8]), .Ci(c2), .S(sum[11 : 8]), .Co(c2), .PG(), .GG());
-    CLA4 CLA_inst_4 (.A(A[15 : 12]), .B(B[15 : 12]), .Ci(c3), .S(sum[15 : 12]), .Co(cout), .PG(), .GG());
+    wire PG0,PG1,PG2,PG3;
+    wire GG0,GG1,GG2,GG3;
+    wire c1,c2,c3;
+    CLA4 CLA_inst_1 (.A(A[3 : 0]), .B(B[3 : 0]), .Ci(Ci), .S(sum[3 : 0]), .Co(), .PG(PG0), .GG(GG0));
+    CLA4 CLA_inst_2 (.A(A[7 : 4]), .B(B[7 : 4]), .Ci(c1), .S(sum[7 : 4]), .Co(), .PG(PG1), .GG(GG1));
+    CLA4 CLA_inst_3 (.A(A[11 : 8]), .B(B[11 : 8]), .Ci(c2), .S(sum[11 : 8]), .Co(), .PG(PG2), .GG(GG2));
+    CLA4 CLA_inst_4 (.A(A[15 : 12]), .B(B[15 : 12]), .Ci(c3), .S(sum[15 : 12]), .Co(cout), .PG(PG3), .GG(GG3));
+    assign c1 = GG0 || (PG0 && Ci); 
+    assign c2 = GG1 || (PG1 && GG0) || (PG1 && PG0 && Ci);
+    assign c3 = GG2 || (PG2 && GG1) || (PG2 && PG1 && GG0) || (PG2 && PG1 && PG0 && Ci);
 endmodule
--- Source/im2col.c/im2col.c
+++ Source/im2col.c/im2col.c
@@ -1,29 +1,57 @@
 #include <stdio.h>
 
 #define DATA_WIDTH 8
-#define IN_X 16
-#define IN_Y 16
-#define IC 32
-#define OC 64
+#define IN_X 8//16
+#define IN_Y 8//16
+#define IC 8//32
+#define OC 16//64
 #define K 3
-#define OUT_X 288
-#define OUT_Y 196
+#define OUT_X 72//288
+#define OUT_Y 36//196
 
 
 int main(void) {
     unsigned int IN_TENSOR[IC][IN_X][IN_Y];
+    for(int i = 0; i < IC ; i++) {
+        for(int k = 0; k < IN_X; k++){
+            for(int l = 0; l <IN_Y; l++){
+                IN_TENSOR[i][k][l] = (l+1)+(k*IN_X)+i;
+            }
+        }
+    }
+    for(int i = 0; i < IC ; i++) {
+        for(int k = 0; k < IN_X; k++){
+            for(int l = 0; l <IN_Y; l++){
+                printf("%-2d ",IN_TENSOR[i][k][l]);
+            }
+            printf("\n");
+        }
+        printf("\n\n\n");
+    }
     unsigned int OUT_MATRIX[OUT_X][OUT_Y];
     /*
     for(){
         Xil_In32()
     };
     */
+   unsigned int in_channel;
+   unsigned int row;
+   unsigned int col;
     for(int i = 0; i < OUT_Y; i++){
         for(int k = 0; k < OUT_X; k++){
-            in_channel = k % (K^2)
-            OUT_MATRIX[k][i] = IN_TENSOR[in_channel][];
+            in_channel = k / (K*K);
+            row = (i / 6) + (k / K) % K; // change 14 to variable
+            col = (k % K) + (i % 6);
+            OUT_MATRIX[k][i] = IN_TENSOR[in_channel][row][col];
         };
-      
+    }
+    for(int i = 0; i < OUT_X; i++){
+        for(int k = 0; k < OUT_Y; k++){
+           printf("%-2d ",OUT_MATRIX[i][k]);
+        };
+        printf("\n");
+        if(i!=0 && i%9 == 8)
+            printf("\n");
     }
     return 0;
 }
