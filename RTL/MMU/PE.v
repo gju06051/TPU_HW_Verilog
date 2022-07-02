3b41e41bf11b7cd07299d23917183ba6934e1184
gju06051 <gju06051@gmail.com>
2022-07-02 09:59:38 +0900

    pe

--- RTL/MMU/PE.v
+++ RTL/MMU/PE.v
@@ -1,18 +1,71 @@
 module PE #(
-    // parameter
+    // Parameter
     parameter DATA_WIDTH = 8,
-    parameter IF_ROW_WIDTH = 15,
-    parameter FILTER_ROW_WIDTH = 3
+    parameter PSUM_WIDTH = 32
     )
     (
-    // port
+    // Special Input
+    input   clk;
+    input   rst_n;
     
+    // Primitives(input)
+    input   [DATA_WIDTH-1:0]    ifmap_i;
+    input   [DATA_WIDTH-1:0]    weight_i;
+    input   [PSUM_WIDTH-1:0]    psum_i;
+    
+    // Register enable signal(output)
+    input                       weight_en_i;
+    input                       ifmap_en_i;
+    input                       psum_en_i;
+    
+    // Primitives(input)
+    output  [DATA_WIDTH-1:0]    ifmap_o;
+    output  [DATA_WIDTH-1:0]    weight_o;
+    output  [PSUM_WIDTH-1:0]    psum_o;
+
+    // Register enable signal(output)
+    output                      weight_en_o;
+    output                      ifmap_en_o;
+    output                      psum_en_o;    
     );
+    
+    // temp signal 
+    wire    [DATA_WIDTH-1:0]        weight_w;
+    wire    [DATA_WIDTH-1:0]        ifmap_w;
+    wire    [PSUM_WIDTH-1:0]        psum_w;
+    wire    [(DATA_WIDTH*2)-1:0]    product_w;  // product of weight * ifmap
+    
+    wire                            weight_en_w;
+    wire                            ifmap_en_w;
+    wire                            psum_en_w;
+    
 
-    localparam PSUM_WIDTH = IF_ROW_WIDTH - FILTER_ROW_WIDTH - 1;
+
+    // forwarding control signal(sync with primitives)
+    DFF  #( .DATA_WIDTH(1) ) Weight_en_buf  ( .clk(clk), .rst_n(rst_n), .en('b1), .d(weight_en_i), .q(weight_en_w) );
+    DFF  #( .DATA_WIDTH(1) ) Ifmap_en_buf   ( .clk(clk), .rst_n(rst_n), .en('b1), .d(ifmap_en_i),  .q(ifmap_en_w)  );
+    DFF  #( .DATA_WIDTH(1) ) Psum_en_buf    ( .clk(clk), .rst_n(rst_n), .en('b1), .d(psum_en_i),   .q(psum_en_w)   );
+    
+    // forwarding primitives
+    DFF  #( .DATA_WIDTH(DATA_WIDTH) ) Weight_Reg    ( .clk(clk), .rst_n(rst_n), .en(weight_en_i),   .d(weight_i),   .q(weight_w) );
+    DFF  #( .DATA_WIDTH(DATA_WIDTH) ) Ifmap_Reg     ( .clk(clk), .rst_n(rst_n), .en(ifmap_en_i),    .d(ifmap_i),    .q(ifmap_w)  );
+    DFF  #( .DATA_WIDTH(PSUM_WIDTH) ) Psum_Reg      ( .clk(clk), .rst_n(rst_n), .en(psum_en_i),     .d(psum_i),     .q(psum_w)   );
 
 
+    // multiplication
+    eight_bit_multiplier_wallace multiplier( .A(ifmap_i), .B(weight_w), .Product(product_w) );
 
 
+    // accumulation(using dedicatied logic)
+    assign psum_o = product_w + psum_w;
+
+    // forwarding(primitives)
+    assign weight_o = weight_w;
+    assign ifmap_o  = ifmap_w;
+    
+    // forwarding(control signal)
+    assign weight_en_o  = weight_en_w;
+    assign ifmap_en_o   = ifmap_en_w;
+    assign psum_en_o    = psum_en_w;
 
 endmodule
---
+++ SIM/PE_tb.v
@@ -0,0 +1,144 @@
+`timescale 1ps/1ps
+`define DELTA 3
+`define CLOCK_PERIOD 10
+
+module PE_TB #(
+    parameter DATA_WIDTH = 8,
+    parameter PSUM_WIDTH = 32
+    )
+    (
+    // no inout
+    // this is testbench
+    );
+    
+    // Special Input
+    reg   clk;
+    reg   rst_n;
+    
+    // Primitives(input)
+    reg   [DATA_WIDTH-1:0]    weight_i;
+    reg   [DATA_WIDTH-1:0]    ifmap_i;
+    reg   [PSUM_WIDTH-1:0]    psum_i;
+    
+    // Register enable signal(output)
+    reg                       weight_en_i;
+    reg                       ifmap_en_i;
+    reg                       psum_en_i;
+    
+    // Primitives(input)
+    wire  [DATA_WIDTH-1:0]    weight_o;
+    wire  [DATA_WIDTH-1:0]    ifmap_o;
+    wire  [PSUM_WIDTH-1:0]    psum_o;
+
+    // Register enable signal(output)
+    wire                      weight_en_o;
+    wire                      ifmap_en_o;
+    wire                      psum_en_o; 
+
+    PE #(
+        // Parameter
+        .DATA_WIDTH(DATA_WIDTH), 
+        .PSUM_WIDTH(PSUM_WIDTH) 
+    ) PE_DUT
+    (
+        // Special Input
+        .clk(clk);
+        .rst_n(rst_n);
+        
+        // Primitives(input)
+        .weight_i(weight_i);
+        .ifmap_i(ifmap_i);
+        .psum_i(psum_i);
+        
+        // Register enable signal(output)
+        .weight_en_i(weight_en_i);
+        .ifmap_en_i(ifmap_en_i);
+        .psum_en_i(psum_en_i);
+        
+        // Primitives(input)
+        .ifmap_o(ifmap_o);
+        .weight_o(weight_o);
+        .psum_o(psum_o);
+    
+        // Register enable signal(output)
+        .weight_en_o(weight_en_o);
+        .ifmap_en_o(ifmap_en_o);
+        .psum_en_o(psum_en_o);    
+    );
+    
+    // clock signal
+    initial begin
+        clk = 1'b0;
+        forever begin
+            #(`CLOCK_PERIOD/2) clk = ~clk;
+        end
+    end
+
+    // generate reset signal
+    initial begin
+        rst_n = 1'b1;
+        #(`DELTA)
+        rst_n = 1'b0;
+    end
+    
+    
+    // test stimulus
+    initial begin
+        // initialize
+        RST_N = 1'b1;
+        EN = 1'b0;
+        D = {(DATA_WIDTH){1'b0}};
+        
+        
+        // stimulus
+        
+        // reset signal
+        repeat(3) begin
+            @(posedge CLK);
+        end
+        #(`DELTA)
+            RST_N = 1'b1;
+        #(`DELTA)
+            RST_N = 1'b0;
+        #(`DELTA*3)
+            RST_N = 1'b1;
+        
+        
+        // data in
+        @(posedge CLK);
+        #(`DELTA)
+            D = 'd3;
+            
+        // enable on
+        @(posedge CLK);
+        #(`DELTA)
+            EN = 1'b1;
+        
+        // data change
+        repeat(3) begin
+            @(posedge CLK);
+        end
+        #(`DELTA)
+            D = 'd4;
+        
+        // enable off
+        @(posedge CLK);
+        #(`DELTA)
+            EN = 1'b0;
+            
+        // data change
+        @(posedge CLK);
+        #(`DELTA)
+            D = 'd8;
+        
+        // finish wait 
+        repeat(3) begin
+            @(posedge CLK);
+        end
+        
+        $display("finished testbench");
+        $finish;
+        
+    end
+
+endmodule
