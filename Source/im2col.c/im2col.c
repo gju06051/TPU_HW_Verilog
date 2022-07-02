040985d85c5fcf545ab07a6d8205095e8b038094
sg05060 <psh2018314072@gmail.com>
2022-07-03 02:42:34 +0900

    update im2col and AXI

--- Source/im2col.c/im2col.c
+++ Source/im2col.c/im2col.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdint.h>
 #define DATA_WIDTH 8
 #define IN_X 8//16
 #define IN_Y 8//16
@@ -8,7 +8,14 @@
 #define K 3
 #define OUT_X 72//288
 #define OUT_Y 36//196
+#define PE_SIZE 16
 
+typedef struct  {
+        unsigned int data_0 : 8;
+        unsigned int data_1 : 8;  
+        unsigned int data_2 : 8;
+        unsigned int data_3 : 8;
+    } Flags;
 
 int main(void) {
     unsigned int IN_TENSOR[IC][IN_X][IN_Y];
@@ -52,6 +59,37 @@ int main(void) {
         printf("\n");
         if(i!=0 && i%9 == 8)
             printf("\n");
+    }
+
+    Flags bitfild;
+    int out_col = OUT_Y / PE_SIZE;
+    int out_row = OUT_X / PE_SIZE;
+
+    for(int i = 0; i < out_col; i++){
+        for(int k = 0; k < out_row; k++){
+            for(int l = 0; l < PE_SIZE; l++){
+                for(int m = 0; m < PE_SIZE; m++){
+                    printf("%d %d ",(k*PE_SIZE)+PE_SIZE-1-l,m+(PE_SIZE*i));
+                    switch (m % 4){
+                        case 0:
+                            bitfild.data_3 = OUT_MATRIX[(k*PE_SIZE)+PE_SIZE-1-l][m+(PE_SIZE*i)];
+                            break;
+                        case 1:
+                            bitfild.data_2 = OUT_MATRIX[(k*PE_SIZE)+PE_SIZE-1-l][m+(PE_SIZE*i)];
+                            break;
+                        case 2:
+                            bitfild.data_1 = OUT_MATRIX[(k*PE_SIZE)+PE_SIZE-1-l][m+(PE_SIZE*i)];
+                            break;
+                        case 3:
+                            bitfild.data_0 = OUT_MATRIX[(k*PE_SIZE)+PE_SIZE-1-l][m+(PE_SIZE*i)];
+                            //printf("%d ",bitfild);
+                            //Xil_Out32
+                            break;
+                    };
+                };
+                printf("\n");
+            };
+        };
     }
     return 0;
 }
