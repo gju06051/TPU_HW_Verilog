48baa3eeaa17674748178bb372883e087427ace6
sg05060 <psh2018314072@gmail.com>
2022-07-13 03:17:02 +0900

    update tb_sa_data_mover

--- RTL/GEMM/tb_SA_Data_mover.v
+++ RTL/GEMM/tb_SA_Data_mover.v
@@ -4,8 +4,8 @@ module tb_SA_Data_mover;
     parameter PE_SIZE = 14;
     parameter integer MEM0_DEPTH = 896;
     parameter integer MEM1_DEPTH = 896;
-    parameter integer MEM0_ADDR_WIDTH = 7;
-    parameter integer MEM1_ADDR_WIDTH = 7;
+    parameter integer MEM0_ADDR_WIDTH = 10;
+    parameter integer MEM1_ADDR_WIDTH = 10;
     parameter integer MEM0_DATA_WIDTH = 112;
     parameter integer MEM1_DATA_WIDTH = 112;
     parameter integer OC = 64;
@@ -15,7 +15,10 @@ module tb_SA_Data_mover;
     reg  en;
     reg  [(FIFO_DATA_WIDTH*PE_SIZE)-1:0] rdata_i;
     wire [MEM0_DATA_WIDTH-1:0] mem0_d0;
-
+    /*wire [FIFO_DATA_WIDTH*PE_SIZE-1:0] buffer_1,buffer_2,buffer_3,buffer_4,buffer_5,buffer_6,buffer_7,buffer_8,buffer_9,buffer_10,
+                                        buffer_11,buffer_12,buffer_13,buffer_14;
+                                        
+    assign*/ 
     always #5 clk = ~clk;
     integer i;
     initial begin
@@ -34,9 +37,9 @@ module tb_SA_Data_mover;
                             rdata_i <= {((FIFO_DATA_WIDTH*PE_SIZE)){1'b0}};
                         end
                         else if(i < PE_SIZE) begin
-                            rdata_i <= (rdata_i >> 8) || {{i[FIFO_DATA_WIDTH-1:0]},{((FIFO_DATA_WIDTH*PE_SIZE)-FIFO_DATA_WIDTH){1'b0}}};
+                            rdata_i <= (rdata_i >> 8) | {{i[FIFO_DATA_WIDTH-1:0]},{((FIFO_DATA_WIDTH*PE_SIZE)-FIFO_DATA_WIDTH){1'b0}}};
                         end else begin
-                            rdata_i <= (rdata_i >> 8) || {{8'b0},{((FIFO_DATA_WIDTH*PE_SIZE)-FIFO_DATA_WIDTH){1'b0}}};
+                            rdata_i <= (rdata_i >> 8) | {{8'b0},{((FIFO_DATA_WIDTH*PE_SIZE)-FIFO_DATA_WIDTH){1'b0}}};
                         end
             end
     end
