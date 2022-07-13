f9df65db18cda4fabee7a4c3e032c846263b65be
sg05060 <psh2018314072@gmail.com>
2022-07-13 16:55:14 +0900

    add GEMM

---
+++ RTL/MMU/GEMM.v
@@ -0,0 +1,17 @@
+module GEMM #(
+    // Parameter
+    parameter DATA_WIDTH = 8,
+    parameter PSUM_WIDTH = 32,
+    parameter PE_SIZE = 4,
+    
+    ) 
+    (
+    // Port
+    input clk,
+    input rst_n
+
+
+
+    );
+
+endmodule
