464ca417dc63396d87f9e86af7f8130cef481519
Park Sang Hyun <psh2018314072@gmail.com>
2022-07-14 12:34:07 +0900

    remove bram from TOP_GLB

--- RTL/GEMM/Top_GLB.v
+++ RTL/GEMM/Top_GLB.v
@@ -15,22 +15,32 @@ module Top_GLB # (
     input rst_n,
     input en,
 
+    output wire mem0_ce0,
+    output wire mem0_we0,
+    output wire [MEM0_ADDR_WIDTH-1:0] mem0_addr0,
+    input  wire [MEM0_DATA_WIDTH-1:0] mem0_q0_i,
+
+    output wire mem1_ce0,
+    output wire mem1_we0,
+    output wire [MEM1_ADDR_WIDTH-1:0] mem1_addr0,
+    input  wire [MEM1_DATA_WIDTH-1:0] mem1_q0_i,
+
     output wire [MEM0_DATA_WIDTH-1:0] mem0_q0_o,
     output wire mem0_q0_vaild,
     output [MEM1_DATA_WIDTH-1:0] rdata_o,
     output [PE_SIZE-1:0] weight_en_col_o,
     output wire sa_data_mover_en
 );
-    wire wren_o;
-    wire    mem1_ce0;
-    wire    mem1_we0;
-    wire    mem0_ce0;
-    wire    mem0_we0;
-    wire    [MEM0_ADDR_WIDTH-1:0] mem0_addr0;
-    wire    [MEM1_ADDR_WIDTH-1:0] mem1_addr0;
-    wire   [MEM0_DATA_WIDTH-1:0] mem0_q0_i;
-    wire   [MEM1_DATA_WIDTH-1:0] mem1_q0_i;
-    wire   [MEM1_DATA_WIDTH-1:0] mem1_q0_o;
+    wire    wren_o;
+   // wire    mem1_ce0;
+   // wire    mem1_we0;
+   // wire    mem0_ce0;
+   // wire    mem0_we0;
+   // wire    [MEM0_ADDR_WIDTH-1:0] mem0_addr0;
+   // wire    [MEM1_ADDR_WIDTH-1:0] mem1_addr0;
+   // wire    [MEM0_DATA_WIDTH-1:0] mem0_q0_i;
+   // wire    [MEM1_DATA_WIDTH-1:0] mem1_q0_i;
+    wire    [MEM1_DATA_WIDTH-1:0] mem1_q0_o;
     wire    rden_o;   
     
     Conv_Data_mover_v2 #(
@@ -85,57 +95,4 @@ module Top_GLB # (
         .weight_en_col_o(weight_en_col_o)
     );
 
-    true_dpbram #(
-        .DWIDTH(MEM0_DATA_WIDTH),
-        .AWIDTH(MEM0_ADDR_WIDTH),
-        .MEM_SIZE(MEM0_DEPTH)
-    ) mem0 (
-        /* Special Inputs */
-        .clk(clk),
-
-        /* input for port 0 */
-        .addr0_i(mem0_addr0),
-        .ce0_i(mem0_ce0),
-        .we0_i(mem0_we0),
-        .d0_i(),
-
-        /* input for port 1 */
-        .addr1_i(),
-        .ce1_i(),
-        .we1_i(),
-        .d1_i(),
-
-        /* output for port 0 */
-        .q0_o(mem0_q0_i),
-
-        /* output for port 1 */
-        .q1_o()
-    );
-
-    true_dpbram #(
-        .DWIDTH(MEM0_DATA_WIDTH),
-        .AWIDTH(MEM0_ADDR_WIDTH),
-        .MEM_SIZE(MEM0_DEPTH)
-    ) mem1 (
-        /* Special Inputs */
-        .clk(clk),
-
-        /* input for port 0 */
-        .addr0_i(mem1_addr0),
-        .ce0_i(mem1_ce0),
-        .we0_i(mem1_we0),
-        .d0_i(),
-
-        /* input for port 1 */
-        .addr1_i(),
-        .ce1_i(),
-        .we1_i(),
-        .d1_i(),
-
-        /* output for port 0 */
-        .q0_o(mem1_q0_i),
-
-        /* output for port 1 */
-        .q1_o()
-    );
 endmodule
