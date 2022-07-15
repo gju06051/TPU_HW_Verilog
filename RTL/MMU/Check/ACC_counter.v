7fd79302dafe54e34dc96ea88679649fe192d35a
gju06051 <gju06051@gmail.com>
2022-07-15 15:53:38 +0900

    acc counter update

---
+++ RTL/MMU/Check/ACC_counter.v
@@ -0,0 +1,85 @@
+module ACC_COUNTER #(
+    // Parameter
+    parameter PE_SIZE = 14,
+    parameter WEIGHT_ROW_NUM = 294,
+    parameter WEIGHT_COL_NUM = 70
+    )
+    (
+    // Port
+    
+    // Special Input
+    input   clk,
+    input   rst_n,
+    // Control Input
+    input   psum_en_i,
+    // Valid Output
+    output  ofmap_valid_o
+    );
+
+
+    // Local parameter
+    localparam PSUM_CNT_NUM = WEIGHT_COL_NUM;           // 70
+    localparam PSUM_CNT_WIDTH = $clog2(PSUM_CNT_NUM);
+    localparam ACC_CNT_NUM = WEIGHT_ROW_NUM / PE_SIZE;  // 294 / 14 = 21
+    localparam ACC_CNT_WIDTH = $clog2(ACC_CNT_NUM);
+    
+    
+    
+    // 1. Partial Sum Counter
+    reg [PSUM_CNT_WIDTH-1:0] psum_cnt, psum_cnt_n;
+    
+    // 1-1) Psum counter seq logic
+    always @(posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            psum_cnt <= {(PSUM_CNT_WIDTH){1'b0}};
+        end else begin
+            psum_cnt <= psum_cnt_n;
+        end
+    end
+    
+    // 1-2) Psum counter comb logic
+    always @(*) begin
+        if (psum_cnt==WEIGHT_COL_NUM) begin
+            psum_cnt_n = 0;
+        end else if (psum_en_i) begin
+            psum_cnt_n = psum_cnt + 'd1;
+        end else begin
+            psum_cnt_n = psum_cnt;  // maintain
+        end
+    end
+    
+    // 1-3) generate acc counter enable signal by using psum counter
+    wire psum_cnt_done;
+    assign psum_cnt_done_w = (psum_cnt==WEIGHT_COL_NUM);
+    
+    
+    
+    // 2. Accumulation Counter
+    reg [ACC_CNT_WIDTH-1:0] acc_cnt, acc_cnt_n;
+    
+    // 2-1) Accumulation counter seq logic
+    always @(posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            acc_cnt <= {(ACC_CNT_WIDTH){1'b0}};
+        end else begin
+            acc_cnt <= acc_cnt_n;
+        end
+    end
+    
+    // 2-2) Accumulation counter comb logic
+    always @(*) begin
+        if (psum_cnt_done) begin
+            if (acc_cnt==ACC_CNT_NUM-1) begin
+                acc_cnt_n = 'd0;
+            end else begin
+                acc_cnt_n = acc_cnt + 'd1;
+            end
+        else begin
+            acc_cnt_n = acc_cnt;    // maintain
+        end
+    end
+    
+    // 2-3) output assignment
+    assign ofmap_valid_o = (acc_cnt==ACC_CNT_NUM-1) & psum_en_i;    // ex. acc_cnt = 20, psum_en_i come
+
+endmodule
---
+++ RTL/MMU/Check/ACC_v2.v
@@ -0,0 +1,93 @@
+module ACC_v2 #(
+    // Parameter
+    parameter PE_SIZE       = 4,
+    parameter DATA_WIDTH    = 8,
+    parameter PSUM_WIDTH    = 32,
+    parameter FIFO_DEPTH    = 4,
+    parameter SLICING_IDX   = 32
+    )
+    (
+    // Special input
+    input   clk,
+    input   rst_n,
+    
+    // R/W enable signal
+    input   [PE_SIZE-1:0]   psum_en_i,                  // signal from SA, used for fifo write signal
+    
+    // I/O data
+    input   [PSUM_WIDTH*PE_SIZE-1:0]    psum_row_i,
+    output                              ofmap_valid_o,  // first fifo ofmap valid signal
+    output  [DATA_WIDTH*PE_SIZE-1:0]    ofmap_row_o
+    
+    
+    );
+    
+    // Wire port for data, 2D array var[fifo_col_idx][bit_idx]
+    wire    [PSUM_WIDTH-1:0]    psum_w      [0:PE_SIZE-1];
+    wire    [PSUM_WIDTH-1:0]    fifo_in_w   [0:PE_SIZE-1];  // FIFO wdata
+    wire    [PSUM_WIDTH-1:0]    fifo_out_w  [0:PE_SIZE-1];  // FIFO rdate
+    wire    [PSUM_WIDTH-1:0]    feedback_w  [0:PE_SIZE-1];  // Zero or FIFO rdata
+    
+    // Wire port for control
+    wire    [PE_SIZE-1:0]       rden_w;                     // FIFO read enable signal
+    wire    [PE_SIZE-1:0]       acc_en_w;                   // Accumulation enable signal(use for reset fifo rdata)
+    
+    
+    
+    // Data flow of accumulation FIFO
+    // (fifo_out + psum -> fifo_in)
+    genvar j;
+    generate
+        for (j=0; j < PE_SIZE; j=j+1) begin : GEN_ACC_OP
+            // flatten input data with each wire
+            assign psum_w[j] = psum_row_i[PSUM_WIDTH*(PE_SIZE-j)-1 : PSUM_WIDTH*(PE_SIZE-j-1)];     
+            
+            // checking preload psum, not_full -> preload psum by giving feedback zero
+            assign feedback_w[j] = acc_en_w[j] ? fifo_out_w[j] : {(PSUM_WIDTH){1'b0}};              
+            
+            // FIFO accumulation
+            assign fifo_in_w[j] = feedback_w[j] + psum_w[j];
+            
+            // Read FIFO when accumulation or GLB read activation
+            assign rden_w[j] = (psum_en_i[j] & acc_en_w[j]);
+        end
+    endgenerate
+    
+    
+    // FIFO Inst(i : col num)
+    genvar i;
+    generate
+        for (i=0; i < PE_SIZE; i=i+1) begin : GEN_FIFO
+            FIFO_v2 #(
+                // Parameter
+                .DATA_WIDTH (PSUM_WIDTH),       // data bit width
+                .FIFO_DEPTH (FIFO_DEPTH)        // FIFO entry num
+            ) FIFO_INST (   
+                // special signal
+                .clk        (clk),              // clock signal
+                .rst_n      (rst_n),            // negedge pointer reset signal(don't need to reset data in fifo)
+                // R/W input signal
+                .wren_i     (psum_en_i[i]),     // write enable signal
+                .rden_i     (rden_w[i]),        // read denable signal
+                // F/E output signal
+                .full_o     (acc_en_w[i]),      // check fifo is full, if full the signal is high
+                .empty_o    (),                 // chcek fifo is empty, if empty the signal is high
+                // In/Out data signal
+                .wdata_i    (fifo_in_w[i]),     // write data
+                .rdata_o    (fifo_out_w[i])     // read data
+            );
+        end
+    endgenerate
+    
+    
+    // Output assignment
+    // Output need to be quantized with high 8bit num
+    genvar k;
+    generate
+        for (k=0; k < PE_SIZE; k=k+1) begin : GEN_OUT
+            // concatenate flatten output data
+            assign psum_row_o[DATA_WIDTH*(PE_SIZE-k)-1 : DATA_WIDTH*(PE_SIZE-k-1)] = fifo_out_w[k][SLICING_IDX-1 -: DATA_WIDTH];
+        end
+    endgenerate
+    
+endmodule
